8a54fb0f6c68d8b37ba3f1d052d51a43e8224a25
Sean Bartell <smbarte2@illinois.edu>
2019-01-28 23:33:54 -0600

    CircleCI: attempt to improve LLVM build/cache.

--- default.nix
+++ default.nix
@@ -2,28 +2,36 @@ let
   default_nixpkgs = (import <nixpkgs> {}).fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs-channels";
-    rev = "a70d3bab72ddcc80d45d1150cdaf4e857ff2af0b";
-    sha256 = "04l7jxlv8sp9n61b6irizbpjj8x4c70lpvbn5f2xyh29c5fiqqcz";
+    rev = "11cf7d6e1ffd5fbc09a51b76d668ad0858a772ed";
+    sha256 = "0zcg4mgfdk3ryiqj1j5iv5bljjvsgi6q6j9z1vkq383c4g4clc72";
   };
+  debugLLVM = llvmPackages: (llvmPackages.llvm.override {
+    debugVersion = true;
+  }).overrideAttrs (o: {
+    doCheck = false;
+  });
 in
 { nixpkgs ? default_nixpkgs }:
 
 with import nixpkgs {};
-{
+rec {
   bcdb-llvm4 = callPackage ./build.nix {
-    llvm = llvmPackages_4.llvm.override { debugVersion = true; };
+    llvm = debugLLVM llvmPackages_4;
   };
   bcdb-llvm5 = callPackage ./build.nix {
-    llvm = llvmPackages_5.llvm.override { debugVersion = true; };
+    llvm = debugLLVM llvmPackages_5;
   };
   bcdb-llvm6 = callPackage ./build.nix {
-    llvm = llvmPackages_6.llvm.override { debugVersion = true; };
+    llvm = debugLLVM llvmPackages_6;
   };
   bcdb-llvm7 = callPackage ./build.nix {
-    llvm = llvmPackages_7.llvm.override { debugVersion = true; };
+    llvm = debugLLVM llvmPackages_7;
   };
+
   bcdb-llvm7-clang7 = callPackage ./build.nix {
     inherit (llvmPackages_7) stdenv;
-    llvm = llvmPackages_7.llvm.override { debugVersion = true; };
+    llvm = debugLLVM llvmPackages_7;
   };
+
+  bcdb = bcdb-llvm7;
 }
