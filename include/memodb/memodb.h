f353f8703b36fd915cdf2b515998d7551df91b20
Sean Bartell <smbarte2@illinois.edu>
2019-08-13 21:47:53 -0500

    fix memodb build

--- include/memodb/memodb.h
+++ include/memodb/memodb.h
@@ -32,7 +32,7 @@ public:
   virtual memodb_value *head_get(const char *name) = 0;
   virtual void head_set(llvm::StringRef name, memodb_value *value) = 0;
   virtual ~memodb_db() {}
-  virtual void head_delete(llvm::StringRef name);
+  virtual void head_delete(llvm::StringRef name) = 0;
 };
 
 std::unique_ptr<memodb_db> memodb_db_open(llvm::StringRef uri,
