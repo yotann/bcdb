b5d24f9896a181343f7254b223037665a66f7b93
Sean Bartell <smbarte2@illinois.edu>
2019-08-14 23:04:30 -0500

    try to fix SemaphoreCI again

--- .semaphore/build-deps.sh
+++ .semaphore/build-deps.sh
@@ -4,9 +4,11 @@ set -eu
 nix-store -q --references $(nix-instantiate -A $A) | grep -v 'bcdb$' > .deps.txt
 KEY=nix-realise-$(checksum .deps.txt)
 
+sudo mv /nix /nix.old
 cache restore $KEY
+if ! [ -d /nix ]; then
+  sudo mv /nix.old /nix
 
-if ! [[ $(nix-store --dry-run --realise $(cat .deps.txt)) ]]; then
   nix-store --realise $(cat .deps.txt) | cachix push bcdb
   cache store $KEY-$SEMAPHORE_JOB_ID /nix
   cache list
