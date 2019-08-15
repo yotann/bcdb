b3cac071225bccbcb3543fc2ede50a18663b35a1
Sean Bartell <smbarte2@illinois.edu>
2019-08-14 23:00:13 -0500

    basic working version of bcdb merge

--- lib/BCDB/Merge.cpp
+++ lib/BCDB/Merge.cpp
@@ -1,11 +1,14 @@
 #include "bcdb/BCDB.h"
 
+#include <llvm/ADT/STLExtras.h>
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Linker/IRMover.h>
 #include <llvm/Support/Errc.h>
 #include <llvm/Support/Error.h>
+#include <llvm/Support/ScopedPrinter.h>
+#include <llvm/Transforms/Utils/Cloning.h>
 
 using namespace bcdb;
 using namespace llvm;
@@ -17,7 +20,12 @@ public:
       : bcdb(bcdb), M(std::make_unique<Module>("muxed", bcdb.GetContext())),
         Mover(*M) {}
   Error Add(StringRef Name);
-  std::unique_ptr<Module> TakeModule() { return std::move(M); }
+  std::unique_ptr<Module> TakeModule() {
+    for (GlobalObject &GO : M->global_objects())
+      if (LinkageMap.count(GO.getName()))
+        GO.setLinkage(LinkageMap[GO.getName()]);
+    return std::move(M);
+  }
 
 private:
   BCDB &bcdb;
@@ -25,6 +33,7 @@ private:
   IRMover Mover;
   std::map<std::string, GlobalValue *> LoadedIDs;
   std::map<std::string, std::string> LoadedNames;
+  StringMap<GlobalValue::LinkageTypes> LinkageMap;
 
   Expected<GlobalValue *> LoadID(StringRef ID);
   void MakeWrapper(GlobalValue *GV, StringRef Name);
@@ -32,8 +41,17 @@ private:
 } // end anonymous namespace
 
 void Merger::MakeWrapper(GlobalValue *GV, StringRef Name) {
-  // see llvm::MergeFunctions::writeThunk
+  errs() << "making wrapper " << Name << " for function " << GV->getName()
+         << " in module " << GV->getParent() << "\n";
   Function *F = cast<Function>(GV);
+  if (F->isVarArg()) {
+    // No easy way to do this: https://stackoverflow.com/q/7015477
+    ValueToValueMapTy VMap;
+    Function *G = CloneFunction(F, VMap, /* CodeInfo */ nullptr);
+    G->setName(Name);
+    return;
+  }
+  // see llvm::MergeFunctions::writeThunk
   Function *G =
       Function::Create(F->getFunctionType(), F->getLinkage(), Name, M.get());
   BasicBlock *BB = BasicBlock::Create(G->getContext(), "", G);
@@ -75,6 +93,12 @@ Expected<GlobalValue *> Merger::LoadID(StringRef ID) {
     }
   }
 
+  auto Name = ("__bcdb_id_" + ID).str();
+  if (M->getNamedValue(Name))
+    return make_error<StringError>("name conflict for " + Name,
+                                   errc::invalid_argument);
+  Def->setName(Name);
+
   // Move the definition into the main module.
   if (Error Err = Mover.move(std::move(MPart), {Def},
                              [](GlobalValue &GV, IRMover::ValueAdder Add) {},
@@ -84,7 +108,8 @@ Expected<GlobalValue *> Merger::LoadID(StringRef ID) {
                              /* IsPerformingImport */ false))
     return std::move(Err);
 
-  Result = Def;
+  Result = M->getNamedValue(Name);
+  LinkageMap[Name] = GlobalValue::InternalLinkage;
   return Result;
 }
 
@@ -96,6 +121,12 @@ Error Merger::Add(StringRef Name) {
   if (!RemainderOrErr)
     return RemainderOrErr.takeError();
   std::unique_ptr<Module> Remainder = std::move(*RemainderOrErr);
+
+  // Make all globals external so function modules can link to them.
+  for (GlobalObject &GO : Remainder->global_objects()) {
+    LinkageMap[GO.getName()] = GO.getLinkage();
+    GO.setLinkage(GlobalValue::ExternalLinkage);
+  }
 
   for (const auto &item : PartIDs) {
     StringRef Name = item.first;
@@ -116,19 +147,52 @@ Error Merger::Add(StringRef Name) {
     MakeWrapper(*GVOrErr, Name);
   }
 
-  // - for each unique ID not already loaded:
-  //   - melt into M
-  // - for each function:
-  //   - add wrapper function (rename main)
-  // - melt remainder module into M (rename main)
-
-  // TODO: check whether anything in the remainder will conflict
   std::vector<GlobalValue *> ValuesToLink;
-  for (auto &G : Remainder->globals()) {
+  for (auto &G : concat<GlobalValue>(Remainder->global_objects(), Remainder->aliases(), Remainder->ifuncs())) {
+    errs() << "considering global " << G.getName() << "\n";
     if (!PartIDs.count(G.getName())) {
-      ValuesToLink.push_back(&G);
+      errs() << "linking global " << G << "\n";
+      if (isa<GlobalObject>(G))
+        ValuesToLink.push_back(&G);
+      GlobalValue *G2 = M->getNamedValue(G.getName());
+      if (G2 && !G2->isDeclaration() && !G.isDeclaration()) {
+        auto S = to_string(G), S2 = to_string(*G2);
+        if (S != S2) {
+          return make_error<StringError>("conflicting globals " + S + " and " +
+                                             S2,
+                                         errc::invalid_argument);
+        }
+      }
     }
   }
+
+  for (auto &A : Remainder->aliases()) {
+    errs() << "rewriting alias " << A << "\n";
+    if (auto GV = dyn_cast<GlobalValue>(A.getAliasee()->stripPointerCasts())) {
+      Constant *GV2;
+      if (GV->getValueType()->isFunctionTy()) {
+        GV2 = M->getOrInsertFunction(GV->getName(),
+                                     cast<FunctionType>(GV->getValueType()));
+      } else {
+        GV2 = M->getOrInsertGlobal(GV->getName(), GV->getValueType());
+      }
+      if (GV2->getType() != A.getType())
+        GV2 = ConstantExpr::getBitCast(GV2, A.getType());
+      auto A2 = GlobalAlias::create(A.getValueType(), 0, A.getLinkage(),
+                                    A.getName(), GV2, M.get());
+      GlobalValue *Old = M->getNamedValue(A.getName());
+      if (Old != A2) {
+        Old->replaceAllUsesWith(A2);
+        Old->eraseFromParent();
+        A2->setName(A.getName());
+      }
+      errs() << "got " << *A2 << "\n";
+    } else {
+      return make_error<StringError>("unsupported alias " + A.getName(),
+                                     errc::invalid_argument);
+    }
+  }
+
   Error Err = Mover.move(std::move(Remainder), ValuesToLink,
                          [](GlobalValue &GV, IRMover::ValueAdder Add) {},
 #if LLVM_VERSION_MAJOR <= 4
@@ -137,6 +201,14 @@ Error Merger::Add(StringRef Name) {
                          /* IsPerformingImport */ false);
   if (Err)
     return std::move(Err);
+
+  // errs() << *M;
+
+  auto Main = M->getNamedValue("main");
+  if (Main) {
+    Main->setName("__bcdb_main_" + Name);
+    LoadedNames.erase("main");
+  }
 
   return Error::success();
 }
@@ -148,7 +220,9 @@ BCDB::Merge(std::vector<StringRef> Names,
   for (StringRef Name : Names) {
     Error Err = Merger.Add(Name);
     if (Err)
-      return Err;
+      return std::move(Err);
   }
-  return Merger.TakeModule();
+  auto M = Merger.TakeModule();
+  // errs() << *M;
+  return std::move(M);
 }
