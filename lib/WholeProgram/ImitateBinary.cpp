103683912879fd6918e82d4f3340b5193669b331
Sean Bartell <smbarte2@illinois.edu>
2019-10-22 21:23:04 -0500

    bc-imitate: fix for LLVM<7

--- lib/WholeProgram/ImitateBinary.cpp
+++ lib/WholeProgram/ImitateBinary.cpp
@@ -1,5 +1,6 @@
 #include "bcdb/WholeProgram.h"
 
+#include <algorithm>
 #include <llvm/IR/Metadata.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Object/ELFObjectFile.h>
@@ -16,14 +17,48 @@ using namespace llvm::object;
 template <class ELFT>
 static bool ImitateELF(Module &M, const ELFObjectFile<ELFT> &ELF) {
   ExitOnError Err("ImitateELF: ");
+
+#if LLVM_VERSION_MAJOR >= 7
   auto DynamicEntries = Err(ELF.getELFFile()->dynamicEntries());
+  auto toMappedAddr = [&](uint64_t VAddr) -> const uint8_t * {
+    return Err(ELF.getELFFile()->toMappedAddr(VAddr));
+  };
+#else
+  using Elf_Dyn = typename ELFFile<ELFT>::Elf_Dyn;
+  using Elf_Phdr = typename ELFFile<ELFT>::Elf_Phdr;
+  ArrayRef<Elf_Dyn> DynamicEntries;
+  SmallVector<const Elf_Phdr *, 4> LoadSegments;
+  for (const Elf_Phdr &Phdr : Err(ELF.getELFFile()->program_headers())) {
+    if (Phdr.p_type == ELF::PT_DYNAMIC) {
+      // TODO: check for errors
+      const Elf_Dyn *start = reinterpret_cast<const Elf_Dyn *>(
+          ELF.getELFFile()->base() + Phdr.p_offset);
+      const Elf_Dyn *end = start + Phdr.p_filesz / sizeof(Elf_Dyn);
+      DynamicEntries = ArrayRef<Elf_Dyn>(start, end);
+    } else if (Phdr.p_type == ELF::PT_LOAD && Phdr.p_filesz) {
+      LoadSegments.push_back(&Phdr);
+    }
+  }
+  auto toMappedAddr = [&](uint64_t VAddr) -> const uint8_t * {
+    const Elf_Phdr *const *I = std::upper_bound(
+        LoadSegments.begin(), LoadSegments.end(), VAddr, compareAddr<ELFT>);
+    if (I == LoadSegments.begin())
+      report_fatal_error("Virtual address is not in any segment");
+    --I;
+    const Elf_Phdr &Phdr = **I;
+    uint64_t Delta = VAddr - Phdr.p_vaddr;
+    if (Delta >= Phdr.p_filesz)
+      report_fatal_error("Virtual address is not in any segment");
+    return ELF.getELFFile()->base() + Phdr.p_offset + Delta;
+  };
+#endif
+
   const char *strtab = nullptr;
   ssize_t rpath = -1;
   SmallVector<size_t, 8> needed;
   for (auto &Dyn : DynamicEntries) {
     if (Dyn.d_tag == ELF::DT_STRTAB) {
-      auto MappedAddr = Err(ELF.getELFFile()->toMappedAddr(Dyn.getPtr()));
-      strtab = reinterpret_cast<const char *>(MappedAddr);
+      strtab = reinterpret_cast<const char *>(toMappedAddr(Dyn.getPtr()));
     } else if (Dyn.d_tag == ELF::DT_NEEDED) {
       needed.push_back(Dyn.d_un.d_val);
     } else if (Dyn.d_tag == ELF::DT_RPATH || Dyn.d_tag == ELF::DT_RUNPATH) {
