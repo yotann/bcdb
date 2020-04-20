be4e29731abf23e7d419ab856d6b2ea37f90347b
Sean Bartell <smbarte2@illinois.edu>
2020-04-20 13:03:52 -0500

    WholeProgram: require LLVM 7.0

--- lib/WholeProgram/ImitateBinary.cpp
+++ lib/WholeProgram/ImitateBinary.cpp
@@ -18,44 +18,10 @@ template <class ELFT>
 static bool ImitateELF(Module &M, const ELFObjectFile<ELFT> &ELF) {
   ExitOnError Err("ImitateELF: ");
 
-#if LLVM_VERSION_MAJOR >= 7
   auto DynamicEntries = Err(ELF.getELFFile()->dynamicEntries());
   auto toMappedAddr = [&](uint64_t VAddr) -> const uint8_t * {
     return Err(ELF.getELFFile()->toMappedAddr(VAddr));
   };
-#else
-  // based on old versions of llvm/tools/llvm-readobj/ELFDumper.cpp
-  using Elf_Dyn = typename ELFFile<ELFT>::Elf_Dyn;
-  using Elf_Phdr = typename ELFFile<ELFT>::Elf_Phdr;
-  ArrayRef<Elf_Dyn> DynamicEntries;
-  SmallVector<const Elf_Phdr *, 4> LoadSegments;
-  for (const Elf_Phdr &Phdr : Err(ELF.getELFFile()->program_headers())) {
-    if (Phdr.p_type == ELF::PT_DYNAMIC) {
-      // TODO: check for errors
-      const Elf_Dyn *start = reinterpret_cast<const Elf_Dyn *>(
-          ELF.getELFFile()->base() + Phdr.p_offset);
-      const Elf_Dyn *end = start + Phdr.p_filesz / sizeof(Elf_Dyn);
-      DynamicEntries = ArrayRef<Elf_Dyn>(start, end);
-    } else if (Phdr.p_type == ELF::PT_LOAD && Phdr.p_filesz) {
-      LoadSegments.push_back(&Phdr);
-    }
-  }
-  auto toMappedAddr = [&](uint64_t VAddr) -> const uint8_t * {
-    const Elf_Phdr *const *I =
-        std::upper_bound(LoadSegments.begin(), LoadSegments.end(), VAddr,
-                         [](uint64_t VAddr, const Elf_Phdr_Impl<ELFT> *Phdr) {
-                           return VAddr < Phdr->p_vaddr;
-                         });
-    if (I == LoadSegments.begin())
-      report_fatal_error("Virtual address is not in any segment");
-    --I;
-    const Elf_Phdr &Phdr = **I;
-    uint64_t Delta = VAddr - Phdr.p_vaddr;
-    if (Delta >= Phdr.p_filesz)
-      report_fatal_error("Virtual address is not in any segment");
-    return ELF.getELFFile()->base() + Phdr.p_offset + Delta;
-  };
-#endif
 
   const char *strtab = nullptr;
   ssize_t rpath = -1;
