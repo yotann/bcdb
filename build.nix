9392cb9ecc2af72302692bf408527945c3ccf9a8
Sean Bartell <smbarte2@illinois.edu>
2020-05-05 21:30:41 -0500

    build.nix: use the same source name for any path

--- build.nix
+++ build.nix
@@ -1,14 +1,22 @@
 { stdenv, nix-gitignore, clang, cmake, libsodium, llvm, pkgconfig, python2, sqlite, xxd }:
 
-stdenv.mkDerivation {
+let
+  gitFilter = patterns: root: with nix-gitignore;
+      gitignoreFilterPure (_: _: true) (withGitignoreFile patterns root) root;
+
+in stdenv.mkDerivation {
   name = "bcdb";
   version = "0.0.1";
 
-  src = nix-gitignore.gitignoreSource [''
-    .*
-    *.nix
-    /nix/
-  ''] ./.;
+  src = builtins.path {
+    path = ./.;
+    name = "bcdb-source";
+    filter = gitFilter [''
+      .*
+      *.nix
+      /nix/
+    ''] ./.;
+  };
 
   nativeBuildInputs = [ clang cmake pkgconfig python2 xxd ];
   buildInputs = [ libsodium llvm sqlite ];
