ced8ada3284fa9001e9fb6241746f6bec8e341bf
Sean Bartell <smbarte2@illinois.edu>
2021-02-07 18:56:59 -0600

    smout: candidates subcommand

--- tools/smout/smout.cpp
+++ tools/smout/smout.cpp
@@ -2,9 +2,11 @@
 #include <llvm/ADT/StringRef.h>
 #include <llvm/Bitcode/BitcodeWriter.h>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Verifier.h>
 #include <llvm/IRReader/IRReader.h>
+#include <llvm/InitializePasses.h>
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/Errc.h>
 #include <llvm/Support/Error.h>
@@ -16,20 +18,26 @@
 #include <llvm/Support/SystemUtils.h>
 #include <llvm/Support/ToolOutputFile.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Transforms/Utils/ModuleUtils.h>
 #include <memory>
 #include <string>
 
 #include "bcdb/BCDB.h"
 #include "bcdb/LLVMCompat.h"
+#include "bcdb/Outlining/Candidates.h"
+#include "bcdb/Outlining/Dependence.h"
+#include "bcdb/Outlining/Extractor.h"
 #include "memodb/memodb.h"
 
 using namespace bcdb;
 using namespace llvm;
 
-static cl::SubCommand CandidatesCommand("candidates", "Generate outlineable candidates");
+static cl::SubCommand CandidatesCommand("candidates",
+                                        "Generate outlineable candidates");
 
-static cl::opt<std::string> ModuleName("name", cl::desc("Name of the head to work on"),
-                                    cl::sub(CandidatesCommand));
+static cl::opt<std::string> ModuleName("name",
+                                       cl::desc("Name of the head to work on"),
+                                       cl::sub(CandidatesCommand));
 
 static cl::opt<std::string> UriOrEmpty(
     "uri", cl::Optional, cl::desc("URI of the database"),
@@ -50,6 +58,75 @@ static StringRef GetUri() {
 static int Candidates() {
   ExitOnError Err("smout candidates: ");
   std::unique_ptr<BCDB> db = Err(BCDB::Open(GetUri()));
+  for (auto &FuncId : Err(db->ListFunctionsInModule(ModuleName))) {
+    auto M = Err(db->GetFunctionById(FuncId));
+    Function *Def = nullptr;
+    for (Function &F : *M) {
+      if (!F.isDeclaration()) {
+        if (Def) {
+          Err(make_error<StringError>("multiple functions in function module",
+                                      errc::invalid_argument));
+        }
+        Def = &F;
+      }
+    }
+
+    legacy::PassManager PM;
+    PM.add(new OutliningExtractorWrapperPass());
+    PM.run(*M);
+
+    // Make sure all functions are named so we can track them after saving.
+    nameUnamedGlobals(*M);
+
+    SmallVector<memodb_value, 8> NodesValues;
+    SmallVector<std::string, 8> CalleeNames;
+    SmallVector<std::string, 8> CallerNames;
+
+    NamedMDNode *NMD = M->getNamedMetadata("smout.extracted.functions");
+    if (NMD && NMD->getNumOperands()) {
+      for (auto &Candidate :
+           cast<MDNode>(*NMD->getOperand(0)->getOperand(1)).operands()) {
+        // [Nodes, callee, caller]
+        MDNode &Node = cast<MDNode>(*Candidate);
+        ConstantDataSequential &NodesArray = cast<ConstantDataSequential>(
+            *cast<ConstantAsMetadata>(*Node.getOperand(0)).getValue());
+        Constant *Callee =
+            cast<ConstantAsMetadata>(*Node.getOperand(1)).getValue();
+        Constant *Caller =
+            cast<ConstantAsMetadata>(*Node.getOperand(2)).getValue();
+        if (Callee->isNullValue() || Caller->isNullValue())
+          continue; // ignore unsupported sequences
+
+        CalleeNames.push_back(Callee->getName().str());
+        CallerNames.push_back(Caller->getName().str());
+        memodb_value NodesValue = memodb_value::array();
+        for (size_t i = 0; i < NodesArray.getNumElements(); i++)
+          NodesValue.array_items().push_back(NodesArray.getElementAsInteger(i));
+        NodesValues.push_back(std::move(NodesValue));
+      }
+    }
+
+    Err(db->Add("__outlined", std::move(M)));
+
+    memodb_value Module = db->get_db().get(db->get_db().head_get("__outlined"));
+    memodb_value Functions = Module.map_items()["functions"];
+
+    memodb_value Result = memodb_value::array();
+    for (size_t i = 0; i < NodesValues.size(); i++) {
+      Result.array_items().push_back(memodb_value::map({
+          {"nodes", std::move(NodesValues[i])},
+          {"callee",
+           std::move(
+               Functions.map_items()[memodb_value::bytes(CalleeNames[i])])},
+          {"caller",
+           std::move(
+               Functions.map_items()[memodb_value::bytes(CallerNames[i])])},
+      }));
+    }
+
+    db->get_db().call_set("smout.candidates", {memodb_ref{FuncId}},
+                          db->get_db().put(Result));
+  }
   return 0;
 }
 
