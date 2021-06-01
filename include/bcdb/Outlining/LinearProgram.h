20692576de6c05062a60f390a0abfaf502d2e830
Sean Bartell <smbarte2@illinois.edu>
2021-05-31 19:15:41 -0500

    add LinearProgram class

---
+++ include/bcdb/Outlining/LinearProgram.h
@@ -0,0 +1,137 @@
+#ifndef BCDB_LINEARPROGRAM_H
+#define BCDB_LINEARPROGRAM_H
+
+#include <llvm/ADT/StringMap.h>
+#include <llvm/ADT/StringRef.h>
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace llvm {
+class raw_ostream;
+}
+
+namespace bcdb {
+
+class LinearProgram {
+public:
+  class Var {
+    size_t ID;
+    Var() = delete;
+    Var(size_t ID) : ID(ID) {}
+
+    friend class LinearProgram;
+  };
+
+  class Expr {
+    friend class LinearProgram;
+
+    std::vector<std::pair<Var, double>> Items = {};
+    double Constant = 0;
+
+  public:
+    Expr() {}
+    Expr(double Constant) : Constant(Constant) {}
+    Expr(Var X) : Items{{X, 1.0}} {}
+
+    Expr &operator+=(const Expr &Other);
+    Expr &operator-=(const Expr &Other);
+    Expr &operator*=(double Other);
+  };
+
+  class Constraint {
+    friend class LinearProgram;
+    friend LinearProgram::Constraint operator<=(LinearProgram::Expr &&A,
+                                                const LinearProgram::Expr &B);
+    friend LinearProgram::Constraint operator>=(LinearProgram::Expr &&A,
+                                                const LinearProgram::Expr &B);
+    friend LinearProgram::Constraint operator==(LinearProgram::Expr &&A,
+                                                const LinearProgram::Expr &B);
+
+    Expr LHS;
+    enum { LE, GE, EQ } Type;
+  };
+
+  LinearProgram(llvm::StringRef Name);
+  void writeFreeMPS(llvm::raw_ostream &OS);
+
+  void addConstraint(llvm::StringRef Name, Constraint &&Constraint) {
+    ConstraintNames.emplace_back(Name);
+    Constraints.emplace_back(std::move(Constraint));
+  }
+
+  void setObjective(llvm::StringRef Name, Expr &&Objective) {
+    ObjectiveName = Name;
+    this->Objective = std::move(Objective);
+  }
+
+  Var makeBoolVar(llvm::StringRef Name);
+  Var makeIntVar(llvm::StringRef Name, std::optional<int> LowerBound,
+                 std::optional<int> UpperBound);
+  Var makeRealVar(llvm::StringRef Name, std::optional<double> LowerBound,
+                  std::optional<double> UpperBound);
+
+private:
+  struct VarInfo {
+    std::string Name;
+    bool IsInteger;
+    std::optional<double> LowerBound, UpperBound;
+  };
+
+  std::string Name;
+  std::vector<VarInfo> Vars;
+  std::string ObjectiveName;
+  Expr Objective;
+  std::vector<std::string> ConstraintNames;
+  std::vector<Constraint> Constraints;
+};
+
+inline LinearProgram::Expr operator+(LinearProgram::Expr &&A,
+                                     const LinearProgram::Expr &B) {
+  A += B;
+  return A;
+}
+
+inline LinearProgram::Expr operator-(LinearProgram::Expr &&A,
+                                     const LinearProgram::Expr &B) {
+  A -= B;
+  return A;
+}
+
+inline LinearProgram::Expr operator*(LinearProgram::Expr &&A, double B) {
+  A *= B;
+  return A;
+}
+
+inline LinearProgram::Expr operator*(double A, LinearProgram::Expr &&B) {
+  B *= A;
+  return B;
+}
+
+inline LinearProgram::Constraint operator<=(LinearProgram::Expr &&A,
+                                            const LinearProgram::Expr &B) {
+  LinearProgram::Constraint Result;
+  Result.Type = LinearProgram::Constraint::LE;
+  Result.LHS = std::move(A) - B;
+  return Result;
+}
+
+inline LinearProgram::Constraint operator>=(LinearProgram::Expr &&A,
+                                            const LinearProgram::Expr &B) {
+  LinearProgram::Constraint Result;
+  Result.Type = LinearProgram::Constraint::GE;
+  Result.LHS = std::move(A) - B;
+  return Result;
+}
+
+inline LinearProgram::Constraint operator==(LinearProgram::Expr &&A,
+                                            const LinearProgram::Expr &B) {
+  LinearProgram::Constraint Result;
+  Result.Type = LinearProgram::Constraint::EQ;
+  Result.LHS = std::move(A) - B;
+  return Result;
+}
+
+} // namespace bcdb
+
+#endif // BCDB_LINEARPROGRAM_H
---
+++ unittests/Outlining/LinearProgramTest.cpp
@@ -0,0 +1,88 @@
+#include "bcdb/Outlining/LinearProgram.h"
+
+#include <llvm/Support/raw_ostream.h>
+
+#include "gtest/gtest.h"
+
+using namespace bcdb;
+
+namespace {
+
+TEST(LinearProgramTest, Empty) {
+  LinearProgram LP("Empty");
+  LP.setObjective("COST", {});
+
+  std::string Buffer;
+  llvm::raw_string_ostream OS(Buffer);
+  LP.writeFreeMPS(OS);
+  EXPECT_EQ(OS.str(), "NAME Empty\n"
+                      "ROWS\n"
+                      " N COST\n"
+                      "COLUMNS\n"
+                      "RHS\n"
+                      "BOUNDS\n"
+                      "ENDATA\n");
+}
+
+TEST(LinearProgramTest, Trivial) {
+  LinearProgram LP("Trivial");
+  auto X = LP.makeRealVar("X", 2.0, 3.0);
+  LP.setObjective("COST", X);
+
+  std::string Buffer;
+  llvm::raw_string_ostream OS(Buffer);
+  LP.writeFreeMPS(OS);
+  EXPECT_EQ(OS.str(), "NAME Trivial\n"
+                      "ROWS\n"
+                      " N COST\n"
+                      "COLUMNS\n"
+                      " X COST 1.000000e+00\n"
+                      "RHS\n"
+                      "BOUNDS\n"
+                      " LO BND1 X 2.000000e+00\n"
+                      " UP BND1 X 3.000000e+00\n"
+                      "ENDATA\n");
+}
+
+TEST(LinearProgramTest, Simple) {
+  // Taken from http://lpsolve.sourceforge.net/5.5/mps-format.htm
+  LinearProgram LP("TESTPROB");
+  auto X = LP.makeRealVar("X", 0.0, 4.0);
+  auto Y = LP.makeRealVar("Y", -1.0, 1.0);
+  auto Z = LP.makeRealVar("Z", 0.0, {});
+  LP.addConstraint("LIM1", X + Y <= 5);
+  LP.addConstraint("LIM2", X + Z >= 10);
+  LP.addConstraint("MYEQN", Z - Y == 7);
+  LP.setObjective("COST", X + 4 * Y + 9 * Z);
+
+  std::string Buffer;
+  llvm::raw_string_ostream OS(Buffer);
+  LP.writeFreeMPS(OS);
+  EXPECT_EQ(OS.str(), "NAME TESTPROB\n"
+                      "ROWS\n"
+                      " N COST\n"
+                      " L LIM1\n"
+                      " G LIM2\n"
+                      " E MYEQN\n"
+                      "COLUMNS\n"
+                      " X COST 1.000000e+00\n"
+                      " X LIM1 1.000000e+00\n"
+                      " X LIM2 1.000000e+00\n"
+                      " Y COST 4.000000e+00\n"
+                      " Y LIM1 1.000000e+00\n"
+                      " Y MYEQN -1.000000e+00\n"
+                      " Z COST 9.000000e+00\n"
+                      " Z LIM2 1.000000e+00\n"
+                      " Z MYEQN 1.000000e+00\n"
+                      "RHS\n"
+                      " RHS1 LIM1 5.000000e+00\n"
+                      " RHS1 LIM2 1.000000e+01\n"
+                      " RHS1 MYEQN 7.000000e+00\n"
+                      "BOUNDS\n"
+                      " UP BND1 X 4.000000e+00\n"
+                      " LO BND1 Y -1.000000e+00\n"
+                      " UP BND1 Y 1.000000e+00\n"
+                      "ENDATA\n");
+}
+
+} // end anonymous namespace
