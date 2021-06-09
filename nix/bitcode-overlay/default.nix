04f8266ffec149d322da0318c40592838429104f
Sean Bartell <smbarte2@illinois.edu>
2021-06-08 21:09:01 -0500

    update bitcode-overlay to latest stable nixpkgs

--- nix/bitcode-overlay/default.nix
+++ nix/bitcode-overlay/default.nix
@@ -1,6 +1,6 @@
 (import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/40affc01a03985dacfa3c67b77b49eb7191058c3.tar.gz";
-  sha256 = "1mvdw2908knh4knfyx7av9n2fga725066z5ds2sic1vg25ijd981";
+  url = "https://github.com/NixOS/nixpkgs/archive/5de44c15758465f8ddf84d541ba300b48e56eda4.tar.gz";
+  sha256 = "0nbhqlgfag1l0bmg141nsbw9jh124rxmaim4qzay8c20b2rlrcwn";
 }) {
   overlays = [
     (import ./bitcode.nix)
