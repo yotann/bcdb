42ad3369e336bdbed13fd056b9517d1f6cea1a86
Sean Bartell <smbarte2@illinois.edu>
2021-06-09 12:14:43 -0500

    fix Nixpkgs hash

--- nix/bitcode-overlay/default.nix
+++ nix/bitcode-overlay/default.nix
@@ -1,6 +1,6 @@
 (import (fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/5de44c15758465f8ddf84d541ba300b48e56eda4.tar.gz";
-  sha256 = "0nbhqlgfag1l0bmg141nsbw9jh124rxmaim4qzay8c20b2rlrcwn";
+  sha256 = "05darjv3zc5lfqx9ck7by6p90xgbgs1ni6193pw5zvi7xp2qlg4x";
 }) {
   overlays = [
     (import ./bitcode.nix)
