cc3ae60bd6976d603c111e8d2509bacb95b9819a
Sean Bartell <smbarte2@illinois.edu>
2021-07-22 09:54:22 -0500

    move all NNG code into memodb-server
    NNG won't be used in enough places to give it its own header file.

--- lib/memodb/NNG.cpp
+++
@@ -1,7 +0,0 @@
-#if BCDB_WITH_NNG
-
-#include "memodb/NNG.h"
-
-char memodb::nng::ErrorInfo::ID;
-
-#endif // BCDB_WITH_NNG
--- tools/memodb-server/memodb-server.cpp
+++ tools/memodb-server/memodb-server.cpp
@@ -1,11 +1,18 @@
 #include <cstdlib>
-#include <llvm/ADT/StringRef.h>
-#include <llvm/Support/CommandLine.h>
+#include <memory>
 #include <optional>
+
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/ADT/Twine.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/Error.h>
+#include <nng/nng.h>
+#include <nng/supplemental/http/http.h>
+#include <nng/supplemental/util/platform.h>
 
 #include "memodb/Evaluator.h"
 #include "memodb/HTTP.h"
-#include "memodb/NNG.h"
 #include "memodb/Server.h"
 #include "memodb/Store.h"
 #include "memodb/ToolSupport.h"
@@ -13,6 +20,9 @@
 
 using namespace memodb;
 namespace cl = llvm::cl;
+using llvm::SmallVector;
+using llvm::StringRef;
+using llvm::Twine;
 
 static cl::OptionCategory server_category("MemoDB Server options");
 
@@ -37,44 +47,110 @@ static llvm::StringRef GetStoreUri() {
   return StoreUriOrEmpty;
 }
 
+static void checkErr(int err) {
+  if (err)
+    llvm::report_fatal_error(nng_strerror(err));
+}
+
+namespace {
+
+struct HTTPHandlerDeleter {
+  void operator()(nng_http_handler *handler) { nng_http_handler_free(handler); }
+};
+
+struct HTTPResponseDeleter {
+  void operator()(nng_http_res *res) { nng_http_res_free(res); }
+};
+
+struct HTTPServerDeleter {
+  void operator()(nng_http_server *server) { nng_http_server_release(server); }
+};
+
+struct URLDeleter {
+  void operator()(nng_url *url) { nng_url_free(url); }
+};
+
+}; // end anonymous namespace
+
+std::unique_ptr<nng_url, URLDeleter> parse_url(const Twine &str) {
+  SmallVector<char, 0> buffer;
+  nng_url *result;
+  checkErr(
+      nng_url_parse(&result, str.toNullTerminatedStringRef(buffer).data()));
+  return std::unique_ptr<nng_url, URLDeleter>(result);
+}
+
+std::unique_ptr<nng_http_server, HTTPServerDeleter>
+http_server_hold(nng_url *url) {
+  nng_http_server *result;
+  checkErr(nng_http_server_hold(&result, url));
+  return std::unique_ptr<nng_http_server, HTTPServerDeleter>(result);
+}
+
+std::unique_ptr<nng_http_handler, HTTPHandlerDeleter>
+http_handler_alloc(const Twine &path, void (*func)(nng_aio *)) {
+  nng_http_handler *result;
+  SmallVector<char, 0> buffer;
+  auto path_str = path.toNullTerminatedStringRef(buffer);
+  checkErr(nng_http_handler_alloc(&result, path_str.data(), func));
+  return std::unique_ptr<nng_http_handler, HTTPHandlerDeleter>(result);
+}
+
 namespace {
 struct NNGRequest : public HTTPRequest {
-  nng::HTTPRequestView req;
-  nng::HTTPResponse res;
+  nng_http_req *req;
+  std::unique_ptr<nng_http_res, HTTPResponseDeleter> res;
 
-  NNGRequest(nng::HTTPRequestView req) : req(req) {
-    llvm::ExitOnError Err("memodb-server NNGRequest: ");
-    res = Err(nng::HTTPResponse::alloc());
+  NNGRequest(nng_http_req *req) : req(req) {
+    nng_http_res *res_tmp;
+    checkErr(nng_http_res_alloc(&res_tmp));
+    res.reset(res_tmp);
   }
 
   ~NNGRequest() override {}
 
-  llvm::StringRef getMethodString() const override { return req.getMethod(); }
+  llvm::StringRef getMethodString() const override {
+    return nng_http_req_get_method(req);
+  }
 
   std::optional<URI> getURI() const override {
-    return URI::parse(req.getURI());
+    return URI::parse(nng_http_req_get_uri(req));
   }
 
   std::optional<llvm::StringRef>
   getHeader(const llvm::Twine &key) const override {
-    return req.getHeader(key);
+    SmallVector<char, 0> buffer;
+    auto key_str = key.toNullTerminatedStringRef(buffer);
+    auto result = nng_http_req_get_header(req, key_str.data());
+    if (!result)
+      return std::nullopt;
+    return result;
   }
 
-  llvm::StringRef getBody() const override { return req.getData(); }
+  llvm::StringRef getBody() const override {
+    void *body;
+    size_t size;
+    nng_http_req_get_data(req, &body, &size);
+    return StringRef(reinterpret_cast<const char *>(body), size);
+  }
 
   void sendStatus(std::uint16_t status) override {
-    llvm::ExitOnError Err("memodb-server sendStatus: ");
-    Err(res.setStatus(status));
+    checkErr(nng_http_res_set_status(res.get(), status));
   }
 
   void sendHeader(llvm::StringRef key, const llvm::Twine &value) override {
-    llvm::ExitOnError Err("memodb-server sendHeader: ");
-    Err(res.addHeader(key, value));
+    SmallVector<char, 64> key_buffer, value_buffer;
+    auto key_str = Twine(key).toNullTerminatedStringRef(key_buffer);
+    auto value_str = value.toNullTerminatedStringRef(value_buffer);
+    checkErr(
+        nng_http_res_add_header(res.get(), key_str.data(), value_str.data()));
   }
 
   void sendBody(const llvm::Twine &body) override {
-    llvm::ExitOnError Err("memodb-server sendBody: ");
-    Err(res.copyData(body));
+    SmallVector<char, 0> buffer;
+    auto body_str = body.toStringRef(buffer);
+    checkErr(
+        nng_http_res_copy_data(res.get(), body_str.data(), body_str.size()));
   }
 
   void sendEmptyBody() override { sendHeader("Content-Length", "0"); }
@@ -83,18 +159,12 @@ struct NNGRequest : public HTTPRequest {
 
 static Server *g_server = nullptr;
 
-static void httpHandler(nng_aio *raw_aio) {
-  llvm::ExitOnError Err("memodb-server HTTP handler: ");
-  nng::AIOView aio(raw_aio);
-
-  nng::HTTPRequestView nng_req(
-      reinterpret_cast<nng_http_req *>(aio.getInput(0)));
+static void httpHandler(nng_aio *aio) {
+  auto nng_req = reinterpret_cast<nng_http_req *>(nng_aio_get_input(aio, 0));
   NNGRequest req(nng_req);
-
   g_server->handleRequest(req);
-
-  aio.setOutput(0, req.res.release());
-  aio.finish(0);
+  nng_aio_set_output(aio, 0, req.res.release());
+  nng_aio_finish(aio, 0);
 }
 
 int main(int argc, char **argv) {
@@ -107,18 +177,18 @@ int main(int argc, char **argv) {
   auto evaluator = Evaluator(Store::open(GetStoreUri()));
   Server server(evaluator);
   g_server = &server;
-  auto url = Err(nng::URL::parse(listen_url));
-  auto http_server = Err(nng::HTTPServer::hold(url));
+  auto url = parse_url(listen_url);
+  auto http_server = http_server_hold(url.get());
 
-  auto handler = Err(nng::HTTPHandler::alloc("/", httpHandler));
-  Err(handler.setMethod(std::nullopt));
-  Err(handler.setTree());
-  Err(http_server.addHandler(std::move(handler)));
+  auto handler = http_handler_alloc("/", httpHandler);
+  checkErr(nng_http_handler_set_method(handler.get(), nullptr));
+  checkErr(nng_http_handler_set_tree(handler.get()));
+  checkErr(nng_http_server_add_handler(http_server.get(), handler.release()));
 
-  Err(http_server.start());
+  checkErr(nng_http_server_start(http_server.get()));
 
   while (true) {
-    nng::msleep(1000);
+    nng_msleep(1000);
   }
 
   return 0;
