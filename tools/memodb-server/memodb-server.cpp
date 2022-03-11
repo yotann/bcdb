26756f68854e4583ad59690df0d815880f40aa25
Sean Bartell <smbarte2@illinois.edu>
2022-03-11 09:25:16 -0600

    memodb-server: add Unix socket support

--- tools/memodb-server/memodb-server.cpp
+++ tools/memodb-server/memodb-server.cpp
@@ -34,6 +34,7 @@ namespace beast = boost::beast;
 namespace http = beast::http;
 namespace net = boost::asio;
 using tcp = boost::asio::ip::tcp;
+namespace local = boost::asio::local;
 
 static cl::OptionCategory server_category("MemoDB Server options");
 
@@ -57,15 +58,15 @@ static llvm::StringRef GetStoreUri() {
   return StoreUriOrEmpty;
 }
 
-static std::mutex stdout_mutex;
+static std::mutex g_stdout_mutex;
 
 static Server *g_server = nullptr;
 
 namespace {
-class http_connection;
+class ConnectionBase;
 class http_request : public HTTPRequest {
 public:
-  http_request(std::shared_ptr<http_connection> connection,
+  http_request(std::shared_ptr<ConnectionBase> connection,
                http::request<http::string_body> &request,
                http::response<http::string_body> &response)
       : HTTPRequest(request.method_string(), URI::parse(request.target())),
@@ -98,37 +99,33 @@ public:
   void sendEmptyBody() override;
 
 private:
-  std::shared_ptr<http_connection> connection;
+  std::shared_ptr<ConnectionBase> connection;
   http::request<http::string_body> &request;
   http::response<http::string_body> &response;
 };
 } // end anonymous namespace
 
 namespace {
-class http_connection : public std::enable_shared_from_this<http_connection> {
+class ConnectionBase : public std::enable_shared_from_this<ConnectionBase> {
 public:
-  http_connection(tcp::socket socket) : socket(std::move(socket)) {}
+  ConnectionBase() {}
 
-  void start() { accept_request(); }
+  void start() { readRequest(); }
 
-private:
+protected:
   friend class http_request;
-  tcp::socket socket;
+  template <typename Protocol> friend class Connection;
+  friend class local_connection;
   beast::flat_buffer buffer{8192};
   http::request<http::string_body> request;
   http::response<http::string_body> response;
   std::optional<http_request> wrapped_request;
 
-  void accept_request() {
-    auto self = shared_from_this();
-    http::async_read(socket, buffer, request,
-                     [self](beast::error_code ec, std::size_t) {
-                       if (!ec)
-                         self->process_request();
-                     });
-  }
+  virtual void readRequest() = 0;
+  virtual void writeRemoteAddress(std::ostream &os) const = 0;
+  virtual void writeResponse() = 0;
 
-  void process_request() {
+  void processRequest() {
     wrapped_request.emplace(shared_from_this(), request, response);
     g_server->handleRequest(*wrapped_request);
   }
@@ -141,7 +138,6 @@ private:
     if (response.result_int() >= 200 && response.result_int() <= 299)
       return;
 
-    auto ip_address = socket.remote_endpoint().address().to_string();
     auto body_size = response.body().size();
 
     // TODO: ensure the locale is set correctly.
@@ -150,29 +146,33 @@ private:
     std::strftime(time_buffer, sizeof(time_buffer), "%d/%b/%Y:%H:%M:%S %z",
                   std::localtime(&time));
 
-    std::lock_guard lock(stdout_mutex);
-    std::cout << ip_address << " - - [" << time_buffer << "] \""
-              << request.method_string() << " " << request.target() << " HTTP/"
-              << (request.version() / 10) << "." << (request.version() % 10)
-              << "\" " << response.result_int() << " " << body_size << "\r\n";
+    std::lock_guard lock(g_stdout_mutex);
+    writeRemoteAddress(std::cout);
+    std::cout << " - - [" << time_buffer << "] \"" << request.method_string()
+              << " " << request.target() << " HTTP/" << (request.version() / 10)
+              << "." << (request.version() % 10) << "\" "
+              << response.result_int() << " " << body_size << "\r\n";
   }
 
   void sendResponse() {
-    auto self = shared_from_this();
     wrapped_request->responded = true;
     wrapped_request.reset();
     response.content_length(response.body().size());
     writeLog();
-    self->request = {};
-    http::async_write(socket, response,
-                      [self](beast::error_code ec, std::size_t) {
-                        self->response = {};
-                        if (!ec)
-                          self->accept_request();
-                      });
+    request = {};
+    writeResponse();
   }
 };
 } // end anonymous namespace
+
+template <typename T>
+static void writeEndpoint(std::ostream &os, const T &endpoint) {
+  os << "-";
+}
+
+static void writeEndpoint(std::ostream &os, const tcp::endpoint &endpoint) {
+  os << endpoint.address();
+}
 
 void http_request::sendBody(const llvm::Twine &body) {
   response.body() = body.str();
@@ -184,12 +184,59 @@ void http_request::sendEmptyBody() {
   connection->sendResponse();
 }
 
-static void http_server(tcp::acceptor &acceptor, tcp::socket &socket) {
-  acceptor.async_accept(socket, [&](beast::error_code ec) {
-    if (!ec)
-      std::make_shared<http_connection>(std::move(socket))->start();
-    http_server(acceptor, socket);
-  });
+namespace {
+template <typename Protocol> class Connection : public ConnectionBase {
+public:
+  Connection(typename Protocol::socket socket) : socket(std::move(socket)) {}
+
+protected:
+  void readRequest() override {
+    auto self = shared_from_this();
+    http::async_read(socket, buffer, request,
+                     [self](beast::error_code ec, std::size_t) {
+                       if (!ec)
+                         self->processRequest();
+                     });
+  }
+
+  void writeRemoteAddress(std::ostream &os) const override {
+    writeEndpoint(os, socket.remote_endpoint());
+  }
+
+  void writeResponse() override {
+    auto self = shared_from_this();
+    http::async_write(socket, response,
+                      [self](beast::error_code ec, std::size_t) {
+                        self->response = {};
+                        if (!ec)
+                          self->readRequest();
+                      });
+  }
+
+private:
+  typename Protocol::socket socket;
+};
+} // end anonymous namespace
+
+template <typename Protocol>
+static void asyncAccept(typename Protocol::acceptor &acceptor) {
+  acceptor.async_accept(
+      [&](beast::error_code ec, typename Protocol::socket socket) {
+        if (!ec)
+          std::make_shared<Connection<Protocol>>(std::move(socket))->start();
+        asyncAccept<Protocol>(acceptor);
+      });
+}
+
+template <typename Protocol>
+static void serve(const typename Protocol::endpoint &local_endpoint) {
+  net::io_context ioc{1};
+  typename Protocol::acceptor acceptor{ioc, local_endpoint};
+  asyncAccept<Protocol>(acceptor);
+  // We print this message *after* opening the store (which can take a long
+  // time, if database logs need to be replayed).
+  llvm::errs() << "Server started!\n";
+  ioc.run();
 }
 
 int main(int argc, char **argv) {
@@ -209,17 +256,18 @@ int main(int argc, char **argv) {
     llvm::errs() << "Try http://127.0.0.1:8000/\n";
     return 1;
   }
-  auto const address = net::ip::make_address(uri_or_none->host);
-  unsigned short port = static_cast<unsigned short>(uri_or_none->port);
-  net::io_context ioc{1}; // may call boost::throw_exception
-  tcp::acceptor acceptor{ioc, {address, port}};
-  tcp::socket socket{ioc};
-  http_server(acceptor, socket);
+  net::io_context ioc{1};
+  if (uri_or_none->scheme == "http" || uri_or_none->scheme == "tcp") {
+    auto const address = net::ip::make_address(uri_or_none->host);
+    unsigned short port = static_cast<unsigned short>(uri_or_none->port);
+    serve<tcp>({address, port});
+  } else if (uri_or_none->scheme == "unix") {
+    serve<local::stream_protocol>(uri_or_none->getPathString());
+  } else {
+    llvm::errs() << "Invalid scheme: " << uri_or_none->scheme << "\n";
+    llvm::errs() << "Use http, tcp, or unix\n";
+    return 1;
+  }
 
-  // We print this message *after* opening the store (which can take a long
-  // time, if database logs need to be replayed).
-  llvm::errs() << "Server started!\n";
-
-  ioc.run();
   return 0;
 }
